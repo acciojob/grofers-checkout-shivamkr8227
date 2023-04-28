6619e5dba362c5651d1b8be476c693adceabbbb2
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-28 17:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.querySelectorAll(".price");
+	let totalp=0;
+	prices.forEach((price)=>{
+		totalp=totalp+parseInt(price.innerText);
+	};
+	const newRow=document.createElement("tr");
+	const newCell=document.createElement("td");
+	newCell.colSpan="2";
+	newCell.innerText="Total Price" + totalp;
+	newRow.appendChild(newCell);
+	document.querySelector("table").appendChild(newRow);
+	
   
 };
 
